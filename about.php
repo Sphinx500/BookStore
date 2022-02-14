40c50cc1b5d474f957fc04f30a7d7ae19d224c3a
Sphinx500 <1719210003@utectulancingo.edu.mx>
2022-02-14 12:54:51 -0600

    Created about page

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php
+    include("template//header.php");
+?>
+
+<div class="jumbotron">
+    <h1 class="display-3">About</h1>
+    <p class="lead">Somos una empresa de Desarrollo de Software</p>
+    <hr class="my-2">
+</div>
+
+
+
+<?php
+    include("template//footer.php");
+?>
