2e5a8d0947de5751a71a042f3c187092fa3e919d
Sphinx500 <1719210003@utectulancingo.edu.mx>
2022-02-23 10:48:08 -0600

    Created login form

---
+++ admin/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Administrator</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <br>
+  <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          
+        </div>
+
+          <div class="col-md-4">
+          <div class="card">
+            <div class="card-header">
+              Login
+            </div>
+            <div class="card-body">
+              <form>
+              <div class = "form-group">
+              <label for="exampleInputEmail1">Username</label>
+              <input type="text" class="form-control" name="username" placeholder="Enter username">
+              <small id="userHelp" class="form-text text-muted">We'll never share your access.</small>
+              </div>
+
+              <div class="form-group">
+              <label for="exampleInputPassword1">Password:</label>
+              <input type="password" class="form-control" name="password" placeholder="Password">
+              </div>
+
+              <button type="submit" class="btn btn-primary">Sign In</button>
+              </form> 
+            </div>
+          </div>
+      </div>
+  </div>
+
+  </body>
+</html>
